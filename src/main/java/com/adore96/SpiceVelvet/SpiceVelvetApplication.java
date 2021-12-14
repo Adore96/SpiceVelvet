beb80a52fc7cef301e1577e79e6dfdbb3bc30bdd
Kalhara Perera <adorekasun@gmail.com>
2021-12-15 01:29:03 +0530

    Initial commit

---
+++ src/main/java/com/adore96/SpiceVelvet/SpiceVelvetApplication.java
@@ -0,0 +1,13 @@
+package com.adore96.SpiceVelvet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiceVelvetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpiceVelvetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adore96/SpiceVelvet/SpiceVelvetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adore96.SpiceVelvet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpiceVelvetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
